21e77386e4b63e27f1a92eee05050996f8eeb0ad
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-05-30 23:23:12 +0100

    Add more members to rectangle struct

--- struct.h
+++ struct.h
@@ -15,7 +15,7 @@ struct Rectangle {
 
 	
 
-	float x1, y1, x2, y2, x3, y3, x4, y4;
+	float x1, y1, x2, y2, x3, y3, x4, y4,xc,yc,w,h;
 	SDL_Renderer* renderer;
 
 	Rectangle(SDL_Renderer* renderer,short x,short y,short w,short h) {
@@ -33,6 +33,31 @@ struct Rectangle {
 		y4 = y + h;
 
 		this->renderer = renderer;
+
+		xc = x + w / 2;
+		yc = y + h / 2;
+
+		this->w = w;
+		this->h = h;
+	}
+
+	void set_center(short center_x,short center_y) {
+
+		float dx = center_x - xc;
+		float dy = center_y - yc;
+
+		x1 += dx;
+		x2 += dx;
+		x3 += dx;
+		x4 += dx;
+
+		y1 += dy;
+		y2 += dy;
+		y3 += dy;
+		y4 += dy;
+
+		xc = center_x;
+		yc = center_y;
 	}
 
 	void rotate(short ox,short oy,float angle) {
@@ -41,6 +66,8 @@ struct Rectangle {
 		rotatePoint(x2, y2, ox, oy, angle);
 		rotatePoint(x3, y3, ox, oy, angle);
 		rotatePoint(x4, y4, ox, oy, angle);
+
+		rotatePoint(xc, yc, ox, oy, angle);
 
 		
 	}
